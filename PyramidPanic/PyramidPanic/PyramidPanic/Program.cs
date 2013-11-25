58a8724699a7d0527ce0a77662b3fe526f5a56a0
Kevin <kevin-kentie@hotmail.com>
2013-11-25 12:43:26 +0100

    We hebben nu wat dingen in het logboek gewijzigt tot 12:45

--- PyramidPanic/PyramidPanic/PyramidPanic/Program.cs
+++ PyramidPanic/PyramidPanic/PyramidPanic/Program.cs
@@ -5,12 +5,9 @@ namespace PyramidPanic
 #if WINDOWS || XBOX
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (PyramidPanic game = new PyramidPanic())
             {
                 game.Run();
             }
